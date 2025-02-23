ff1f4d1d47849513e03c6b4414dee1d7b93ebdc4
Kubuses <kubuses11@gmail.com>
2025-02-23 20:31:49 +0100

    added GET controllers added Swagger

---
+++ src/main/java/com/songify/SongsController.java
@@ -0,0 +1,46 @@
+package com.songify;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
+@Log4j2
+public class SongsController {
+
+    Map<Integer, String> database = new HashMap<>();
+
+
+    @GetMapping("/songs")
+    public ResponseEntity<SongResponseDto> getAllSongs(@RequestParam(required = false) Integer limit) {
+        database.put(1, "Shawn Mendes - Stitches");
+        database.put(2, "Ariana Grande - 7 rings");
+        database.put(3, "Billie Eilish - bad guy");
+        database.put(4, "Ed Sheeran - Shape of You");
+        if (limit != null) {
+            Map<Integer, String> limitedMap = database.entrySet()
+                    .stream()
+                    .limit(limit)
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+            SongResponseDto response = new SongResponseDto(limitedMap);
+            return ResponseEntity.ok(response);
+        }
+        SongResponseDto response = new SongResponseDto(database);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/songs/{id}")
+    public ResponseEntity<SingleSongResponseDto> getSongById(@PathVariable Integer id, @RequestHeader(required = false) String requestId) {
+        log.info("Request ID: " + requestId);
+        String song = database.get(id);
+        if (song == null) {
+            return ResponseEntity.notFound().build();
+        }
+        SingleSongResponseDto response = new SingleSongResponseDto(song);
+        return ResponseEntity.ok(response);
+    }
+}
