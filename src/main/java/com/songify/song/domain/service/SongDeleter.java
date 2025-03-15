2cfd371f8ddd52471d9a83b0f8ebcd3a47e1cb80
Kubuses <kubuses11@gmail.com>
2025-03-15 22:45:29 +0100

    - refactoring

--- src/main/java/com/songify/song/domain/service/SongDeleter.java
+++ src/main/java/com/songify/song/domain/service/SongDeleter.java
@@ -1,6 +1,7 @@
 package com.songify.song.domain.service;
 
 import com.songify.song.domain.repository.SongRepository;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 @Service
 @Log4j2
 @AllArgsConstructor
+@Transactional
 public class SongDeleter {
 
     private final SongRepository songRepository;
