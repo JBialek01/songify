f283db0608591fef5f482754185c0518142bdbd2
Kubuses <kubuses11@gmail.com>
2025-04-03 20:32:30 +0200

    Added AlbumInfo interface to optimize sql query when fetching album with songs and artists

---
+++ src/main/java/com/songify/domain/crud/AlbumRetriever.java
@@ -0,0 +1,17 @@
+package com.songify.domain.crud;
+
+import com.songify.domain.crud.dto.AlbumInfo;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+class AlbumRetriever {
+
+    private final AlbumRepository albumRepository;
+
+    AlbumInfo findAlbumByIdWithArtistsAndSongs(final Long id) {
+        return albumRepository.findAlbumByIdWithSongsAndArtists(id)
+                .orElseThrow(() -> new AlbumNotFoundException("Album with id: " + id + " not found"));
+    }
+}
